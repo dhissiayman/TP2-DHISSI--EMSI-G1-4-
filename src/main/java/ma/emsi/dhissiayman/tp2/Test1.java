8d87786bc0417f4b3fa9bb76276c54531d4295f8
Ayman <ubuntutest933@gmail.com>
2025-11-02 14:20:42 +0100

    TEST 1 SIMPLE USAGE OF LANGCHAIN4J WITH GOOGLEAI GEMINI

---
+++ src/main/java/ma/emsi/dhissiayman/tp2/Test1.java
@@ -0,0 +1,29 @@
+package ma.emsi.dhissiayman.tp2;
+
+
+import dev.langchain4j.model.chat.ChatModel;
+// Assurez-vous que l'import correspond bien à votre module LangChain4j :
+import dev.langchain4j.model.googleai.GoogleAiGeminiChatModel;
+import dev.langchain4j.model.input.PromptTemplate;
+import dev.langchain4j.model.input.Prompt;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+        // Renseignez la clé via variable d'environnement GOOGLE_AI_API_KEY
+        ChatModel model = GoogleAiGeminiChatModel.builder()
+                .apiKey(System.getenv("GEMINI_KEY"))
+                .modelName("gemini-2.5-flash")
+                .temperature(0.7)
+                .logRequests(true)
+                .build();
+
+        String question = "Donne-moi des informations sur casablanca";
+
+
+        String response = model.chat(question);
+
+        System.out.println("Question  : " + question);
+        System.out.println("Réponse LLM : " + response);
+    }
+}
